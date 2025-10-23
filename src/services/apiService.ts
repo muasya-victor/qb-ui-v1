56c4039200d03f9131e54543ffea72754b9bfdfe
muasya-victor <vicmwe184@gmail.com>
2025-10-23 21:30:37 +0300

    update api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -97,7 +97,7 @@ class ApiService {
 
   constructor() {
     this.baseUrl =
-      process.env.NEXT_PUBLIC_API_BASE_URL || "http://212.47.77.18/api";
+      process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.v2.smartinvoice.co.ke/api";
   }
 
   private getAuthHeaders(): Record<string, string> {
